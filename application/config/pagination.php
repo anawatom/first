e0b61c3101b8df452feaf62c99adafd917bc6e98
anawatom <anawat.om@gamil.com>
2015-10-10 09:23:33 +0700

    Add the pagination config.
    :)

---
+++ application/config/pagination.php
@@ -0,0 +1,5 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+// $config['base_url'] = 'http://example.com/index.php/test/page/';
+// $config['total_rows'] = 200;
+$config['per_page'] = 30; 
