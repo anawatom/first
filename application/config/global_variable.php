9734672e617966d5b6d48aec645b3550d856275b
anawatom <anawat.om@gamil.com>
2015-10-11 16:23:56 +0700

    Edit the name of variable.
    :)

--- application/config/global_variable.php
+++ application/config/global_variable.php
@@ -1,6 +1,6 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-$config['STATUS_MAPPING'] = array('0' => 'ยกเลิก', '1' => 'ใช้งาน');
+$config['STATUSES'] = array('0' => 'ยกเลิก', '1' => 'ใช้งาน');
 
 /* End of file global_variable.php */
 /* Location: ./application/config/global_variable.php */
