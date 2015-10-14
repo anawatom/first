6dddd809b0fdd9d602cc4c8a1d25cff8a848a78a
anawatom <anawat.om@gamil.com>
2015-10-14 22:13:14 +0700

    Add the language file for the gms_cancel_result.
    :)

---
+++ application/language/thai/gms_cancel_result_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['GMS_CANCEL_RESULT_CANCEL_ID'] = 'รหัสเหตุผลการยกเลิก';
+$lang['GMS_CANCEL_RESULT_CANCEL_CODE'] = 'รหัสเหตุผลการยกเลิก';
+$lang['GMS_CANCEL_RESULT_CANCEL_SUBJECT'] = 'เหตุผลการยกเลิก';
+$lang['GMS_CANCEL_RESULT_CREATE_DATE'] = 'วันที่สร้าง';
+$lang['GMS_CANCEL_RESULT_CREATE_BY'] = 'สร้างโดย';
+$lang['GMS_CANCEL_RESULT_UPDATE_DATE'] = 'วันที่ปรับปรุง';
+$lang['GMS_CANCEL_RESULT_UPDATE_BY'] = 'ปรับปรุงโดย';
+$lang['GMS_CANCEL_RESULT_VERSION'] = 'เวอร์ชันข้อมูล';
+$lang['GMS_CANCEL_RESULT_CANCEL_STATUS'] = 'สถานะ';
+
+/* End of file gms_cancel_result_lang.php */
+/* Location: ./system/language/english/gms_cancel_result_lang.php */
