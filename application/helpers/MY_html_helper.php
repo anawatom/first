8d1b8e9583e8e8bfd9b02a4ce6d325f0b2cc8087
anawatom <anawat.om@gamil.com>
2015-10-17 13:59:58 +0700

    Add the helper function to get image/image preview elements.
    :)

--- application/helpers/MY_html_helper.php
+++ application/helpers/MY_html_helper.php
@@ -38,5 +38,46 @@ if ( ! function_exists('get_element_status'))
 	}
 }
 
+/**
+ * Get the element image to review form the input file.
+ *
+ * @access	public
+ * @param	string
+ * @return	$string
+ */
+if ( ! function_exists('get_element_image_preview'))
+{
+	function get_element_image_preview($src = '', $name = 'IMAGE_PREVIEW')
+	{
+		if ( strlen($src) === 0 )
+		{
+			$src = base_url('img/no_image.png');
+		}
+
+		return '<img class="image-preview" src="'.$src.'" data-name="'.$name.'">';
+	}
+}
+
+/**
+ * Get the element image.
+ *
+ * @access	public
+ * @param	string
+ * @param	string
+ * @return	$string
+ */
+if ( ! function_exists('get_element_image'))
+{
+	function get_element_image($src = '', $class = 'image')
+	{
+		if ( strlen($src) === 0 )
+		{
+			$src = base_url('img/no_image.png');
+		}
+
+		return '<img class="'.$class.'" src="'.$src.'" >';
+	}
+}
+
 /* End of file MY_html_helper.php */
 /* Location: ./system/helpers/MY_html_helper.php */
