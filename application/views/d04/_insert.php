1acbe5aeb79379979ccd76667c7170d967e11ab8
anawatom <anawat.om@gamil.com>
2015-10-18 14:24:28 +0700

    Comment old datepicker.
    :)

--- application/views/d04/_insert.php
+++ application/views/d04/_insert.php
@@ -90,7 +90,7 @@
                                 <td class="form-group has-error"><label>วัน/เดือน/ปี เกิด *</label></td>
                                 <td class="form-group has-error">
 <!--                                            <input type="text" name="BIRTH_DATE" id="BIRTH_DATE" value="" class="form-control txt_input" data-inputmask="'alias': 'dd/mm/yyyy'" data-mask/>-->
-                                    <input type="text" name="BIRTH_DATE" id="BIRTH_DATE" value="<?php echo date('d/m/') . (date('Y') + 543); ?>" class="form-control txt_input" />
+                                    <input type="text" name="BIRTH_DATE" id="BIRTH_DATE" value="<?php echo date('d/m/') . (date('Y') + 543); ?>" class="form-control txt_input datepicker" data-type="birthdate" />
                                 </td>
                                 <td>ศาสนา</td>
                                 <td>
@@ -413,30 +413,29 @@
         //$("#BIRTH_DATE").inputmask("", {"placeholder": "dd/mm/yyyy"});
         //$("[data-mask]").inputmask();
 
-        var thaiYear = function (ct) {
-            var leap = 3;
-            var dayWeek = ["พฤ.", "ศ.", "ส.", "อา.", "จ.", "อ.", "พ."];
-            if (ct) {
-                var yearL = new Date(ct).getFullYear() - 543;
-                leap = (((yearL % 4 == 0) && (yearL % 100 != 0)) || (yearL % 400 == 0)) ? 2 : 3;
-                if (leap == 2) {
-                    dayWeek = ["ศ.", "ส.", "อา.", "จ.", "อ.", "พ.", "พฤ."];
-                }
-            }
-            this.setOptions({
-                i18n: {th: {dayOfWeek: dayWeek}}, dayOfWeekStart: leap,
-            })
-        };
+        // var thaiYear = function (ct) {
+        //     var leap = 3;
+        //     var dayWeek = ["พฤ.", "ศ.", "ส.", "อา.", "จ.", "อ.", "พ."];
+        //     if (ct) {
+        //         var yearL = new Date(ct).getFullYear() - 543;
+        //         leap = (((yearL % 4 == 0) && (yearL % 100 != 0)) || (yearL % 400 == 0)) ? 2 : 3;
+        //         if (leap == 2) {
+        //             dayWeek = ["ศ.", "ส.", "อา.", "จ.", "อ.", "พ.", "พฤ."];
+        //         }
+        //     }
+        //     this.setOptions({
+        //         i18n: {th: {dayOfWeek: dayWeek}}, dayOfWeekStart: leap,
+        //     })
+        // };
 
-        $("#BIRTH_DATE").datetimepicker({
-            timepicker: false,
-            format: 'd/m/Y', // กำหนดรูปแบบวันที่ ที่ใช้ เป็น 00-00-0000              
-            lang: 'th', // แสดงภาษาไทย  
-            onChangeMonth: thaiYear,
-            onShow: thaiYear,
-            yearOffset: 543, // ใช้ปี พ.ศ. บวก 543 เพิ่มเข้าไปในปี ค.ศ  
-            closeOnDateSelect: true,
-        });
+        // $("#BIRTH_DATE").datepicker({
+        //     format: 'd/m/yyyy',
+        //     language: 'th',
+        //     onChangeMonth: thaiYear,
+        //     onShow: thaiYear,
+        //     yearOffset: 543, // ใช้ปี พ.ศ. บวก 543 เพิ่มเข้าไปในปี ค.ศ  
+        //     closeOnDateSelect: true,
+        // });
     });
     document.onkeydown = chkEvent
 
