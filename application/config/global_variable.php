1e932a14553482c9c0ac44c3c54ca16e0088df64
anawatom <anawat.om@gamil.com>
2015-11-01 19:45:03 +0700

    Edit data REPORT_TYPE.
    :)

--- application/config/global_variable.php
+++ application/config/global_variable.php
@@ -2,6 +2,7 @@
 
 $config['UPLOAD_PATH_GMS_CERTIFICATE_SIGN'] = '../files/member/certificate/';
 $config['STATUSES'] = array('0' => 'ยกเลิก', '1' => 'ใช้งาน');
+$config['REPORT_TYPE'] = array('vertical' => 'แนวตั้ง', '1' => 'แนวนอน');
 
 /* End of file global_variable.php */
 /* Location: ./application/config/global_variable.php */
