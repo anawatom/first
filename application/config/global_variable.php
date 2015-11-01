cbde8cd4e1c577a737806f7593d4d239886790df
anawatom <anawat.om@gamil.com>
2015-11-01 20:58:08 +0700

    Edit UPLOAD_PATH_GMS_CERTIFICATE_SIGN.
    :)

--- application/config/global_variable.php
+++ application/config/global_variable.php
@@ -1,6 +1,6 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-$config['UPLOAD_PATH_GMS_CERTIFICATE_SIGN'] = '../files/member/certificate/';
+$config['UPLOAD_PATH_GMS_CERTIFICATE_SIGN'] = '../files/member/';
 $config['STATUSES'] = array('0' => 'ยกเลิก', '1' => 'ใช้งาน');
 $config['REPORT_TYPE'] = array('vertical' => 'แนวตั้ง', 'horizontal' => 'แนวนอน');
 
