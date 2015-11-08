7cafe7591f590e78de4ca028cca836b575334e7d
anawatom <anawat.om@gamil.com>
2015-11-08 11:38:15 +0700

    Add global variable history_status_regis_with_all
    :)

--- application/config/global_variable.php
+++ application/config/global_variable.php
@@ -3,6 +3,12 @@
 $config['UPLOAD_PATH_GMS_CERTIFICATE_SIGN'] = '../files/member/';
 $config['STATUSES'] = array('0' => 'ยกเลิก', '1' => 'ใช้งาน');
 $config['REPORT_TYPE'] = array('vertical' => 'แนวตั้ง', 'horizontal' => 'แนวนอน');
+$config['HISTORY_STATUS_REGIS_WITH_ALL'] = array('all' => 'ทั้งหมด',
+                                        '0' => 'รอพิจารณา',
+                                        '1' => 'อนุมัติ',
+                                        '2' => 'ไม่ผ่านเกณฑ์',
+                                        '3' => 'สละสิทธิ์',
+                                      );
 
 /* End of file global_variable.php */
 /* Location: ./application/config/global_variable.php */
